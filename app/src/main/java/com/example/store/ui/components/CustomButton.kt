2c97c7304cce09593f8750bad977f0d65ab19bfd
Bubnov <nekit.b.nb@mail.ru>
2026-03-03 11:14:59 +0300

    add custom button

--- app/src/main/java/com/example/store/ui/components/CustomButton.kt
+++ app/src/main/java/com/example/store/ui/components/CustomButton.kt
@@ -1,2 +1,32 @@
 package com.example.store.ui.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+/**
+ * Переиспользуемая кнопка
+ * Дата создания: 03.03.2026
+ * Автор: Бубнов Никита
+ */
+@Composable
+fun CustomButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(50.dp),
+        enabled = enabled
+    ) {
+        Text(text = text)
+    }
+}
